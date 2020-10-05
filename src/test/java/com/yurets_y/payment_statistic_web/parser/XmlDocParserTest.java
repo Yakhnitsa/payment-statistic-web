d0db4205a602ed299a867311417ae19229d23efa
Yuriy work-pc <transpmailbox@gmail.com>
2020-10-05 11:31:47 +0300

    wrote grout file test and pass it

--- src/test/java/com/yurets_y/payment_statistic_web/parser/XmlDocParserTest.java
+++ src/test/java/com/yurets_y/payment_statistic_web/parser/XmlDocParserTest.java
@@ -3,9 +3,7 @@ package com.yurets_y.payment_statistic_web.parser;
 
 import com.yurets_y.payment_statistic_web.entity.PaymentDetails;
 import com.yurets_y.payment_statistic_web.entity.PaymentList;
-import com.yurets_y.payment_statistic_web.resources.TestFilesConfig;
 import com.yurets_y.payment_statistic_web.service.parser_services.DocParser;
-import com.yurets_y.payment_statistic_web.service.parser_services.HtmlDocParser;
 import com.yurets_y.payment_statistic_web.service.parser_services.XmlDocParser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,9 +16,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 
 @Import({XmlDocParserTestConfig.class})
 @RunWith(SpringRunner.class)
@@ -28,31 +28,34 @@ public class XmlDocParserTest {
 
     Logger logger = LoggerFactory.getLogger(XmlDocParserTest.class);
 
-    @Resource(name="xml-doc-parser")
+    @Resource(name = "xml-doc-parser")
     private DocParser docParser;
 
-    @Resource(name="test-xml-file")
+    @Resource(name = "test-xml-file")
     private File testXmlFile;
 
+    @Resource(name = "test-xml-directory")
+    private File testXmlDirectory;
+
     @Test
-    public void resourceIntegrationTest(){
+    public void resourceIntegrationTest() {
         assertThat(this.docParser).isNotNull();
     }
 
     @Test
-    public void testFileExists(){
+    public void testFileExists() {
         assertThat(testXmlFile).exists();
     }
 
     @Test
-    public void docNumberAndDateTest() throws IOException{
+    public void docNumberAndDateTest() throws IOException {
         PaymentList paymentList = docParser.parseFromFile(testXmlFile);
         assertThat(paymentList.getNumber()).isEqualTo(20200206);
         assertThat(paymentList.getPayerCode()).isEqualTo(8210260);
     }
 
     @Test
-    public void openingAndClosingBalanceTest() throws IOException{
+    public void openingAndClosingBalanceTest() throws IOException {
         PaymentList paymentList = docParser.parseFromFile(testXmlFile);
         assertThat(paymentList.getOpeningBalance()).isEqualTo(113231014);
         assertThat(paymentList.getPayments()).isEqualTo(308360472);
@@ -62,7 +65,7 @@ public class XmlDocParserTest {
     }
 
     @Test
-    public void parseDeparturePaymentTest() throws IOException{
+    public void parseDeparturePaymentTest() throws IOException {
         PaymentList paymentList = docParser.parseFromFile(testXmlFile);
         long departurePayment = paymentList.getPaymentDetailsList()
                 .stream()
@@ -85,7 +88,7 @@ public class XmlDocParserTest {
     }
 
     @Test
-    public void parseStationPaymentTest() throws IOException{
+    public void parseStationPaymentTest() throws IOException {
         PaymentList paymentList = docParser.parseFromFile(testXmlFile);
         long vagUsagePayment = paymentList.getPaymentDetailsList()
                 .stream()
@@ -102,7 +105,7 @@ public class XmlDocParserTest {
     }
 
     @Test
-    public void parsePaymentsTest() throws IOException{
+    public void parsePaymentsTest() throws IOException {
         PaymentList paymentList = docParser.parseFromFile(testXmlFile);
         long totalPayment = paymentList.getPaymentDetailsList()
                 .stream()
@@ -112,8 +115,8 @@ public class XmlDocParserTest {
     }
 
     @Test
-    public void complexXmlDocParserTest() throws IOException{
-        File file = new File("src/test/resources/test_files/xml/8210260_20200930.xml");
+    public void complexXmlDocParserTest() throws IOException {
+        File file = new File("src/test/resources/test_files/payments/xml/8210260_20200930.xml");
         PaymentList paymentList = docParser.parseFromFile(file);
 
         logger.info("Doc number and code test");
@@ -130,56 +133,93 @@ public class XmlDocParserTest {
 
         logger.info("Payment details tests...");
 
-        long departurePayment = getPaymDetailsSumByType(paymentList.getPaymentDetailsList(),"Відправлення");
+        long departurePayment = getPaymentDetailsSumByType(paymentList.getPaymentDetailsList(), "Відправлення");
         assertThat(departurePayment).isEqualTo(35623600);
 
-        long internDeparturePayment = getPaymDetailsSumByType(paymentList.getPaymentDetailsList(),"Відправлення - міжнародне сполучення");
+        long internDeparturePayment = getPaymentDetailsSumByType(paymentList.getPaymentDetailsList(), "Відправлення - міжнародне сполучення");
         assertThat(internDeparturePayment).isEqualTo(195847970);
 
-        long cumCards = getPaymDetailsSumByType(paymentList.getPaymentDetailsList(),"Накопичувальні карточки");
+        long cumCards = getPaymentDetailsSumByType(paymentList.getPaymentDetailsList(), "Накопичувальні карточки");
         assertThat(cumCards).isEqualTo(3467310);
 
-        long vagPayments = getPaymDetailsSumByType(paymentList.getPaymentDetailsList(),"Відомості плати за користування вагонами");
+        long vagPayments = getPaymentDetailsSumByType(paymentList.getPaymentDetailsList(), "Відомості плати за користування вагонами");
         assertThat(vagPayments).isEqualTo(1179690);
 
-        long infoMessages = getPaymDetailsSumByType(paymentList.getPaymentDetailsList(),"Інформаційні повідомлення");
+        long infoMessages = getPaymentDetailsSumByType(paymentList.getPaymentDetailsList(), "Інформаційні повідомлення");
         assertThat(infoMessages).isEqualTo(827820000);
-
-
-
-//        long deliverTotalPayment = paymentList.getPaymentDetailsList()
-//                .stream()
-//                .filter(pd -> pd.getType().equals("Прибуття"))
-//                .mapToLong(PaymentDetails::getTotalPayment).sum();
-//
-//        assertThat(deliverTotalPayment).isEqualTo(0);
-
-
-
-
-
-
     }
 
-    private long getPaymDetailsSumByType(List<PaymentDetails> pdList, String paymentsType){
+    @Test
+    public void groutDocumentsTest() throws IOException {
+
+        for (File file : testXmlDirectory.listFiles()) {
+            if(file.isFile()){
+                PaymentList paymentList = docParser.parseFromFile(file);
+                checkSumTest(paymentList);
+            }
+
+        }
+    }
+
+    /* Тестирование заведомо неисправного перечня на предмет контрольной суммы*/
+    @Test
+    public void corruptedFileTest() throws IOException {
+        File file = new File("src/test/resources/test_files/payments/xml/corrupted/per_16022020_100001_8210260.xml");
+        PaymentList paymentList = docParser.parseFromFile(file);
+        assertTrue(!paymentList.isTestPassed());
+    }
+
+
+    private long getPaymentDetailsSumByType(List<PaymentDetails> pdList, String paymentsType) {
         return pdList.stream()
                 .filter(pd -> pd.getType().equals(paymentsType))
                 .mapToLong(PaymentDetails::getPayment).sum();
+    }
+
+    private void checkSumTest(PaymentList paymentList) {
+
+
+        long openingBalance = paymentList.getOpeningBalance();
+        long closingBalance = paymentList.getClosingBalance();
+
+        long payments = paymentList.getPaymentDetailsList()
+                .stream()
+                .filter(paymentDetails -> paymentDetails.getIncomeType() == PaymentDetails.IncomeType.INCOME)
+                .mapToLong(PaymentDetails::getTotalPayment).sum();
+        long totalPaymentsVsTaxes = paymentList.getPaymentVsTaxes();
+
+        long totalPaymentsFromList = paymentList.getPaymentDetailsList()
+                .stream()
+                .filter(paymentDetails -> paymentDetails.getIncomeType() == PaymentDetails.IncomeType.OUTCOME)
+                .mapToLong(PaymentDetails::getTotalPayment).sum();
+
+        long checkSum = openingBalance + payments - totalPaymentsFromList;
+
+        boolean totalPaymentsTest = totalPaymentsVsTaxes == totalPaymentsFromList;
+
+        assertTrue(totalPaymentsTest);
+
+        boolean closingBalanceTest = closingBalance == checkSum;
+        assertTrue(closingBalanceTest);
+
+        assertTrue(paymentList.isTestPassed());
+
+        logger.info("Test passed for document: " + paymentList.getPayerCode() + "_" + paymentList.getNumber());
     }
 
 }
 
 class XmlDocParserTestConfig {
 
-    private final String XML_FILE_LOCATION = "src/test/resources/test_files/xml/per_06022020_100001_8210260.xml";
-    private final String XML_FILES_DIRECTORY_LOCATION = "src/test/resources/test_files/xml";
+    private final String XML_FILE_LOCATION = "src/test/resources/test_files/payments/xml/per_06022020_100001_8210260.xml";
+    private final String XML_FILES_DIRECTORY_LOCATION = "src/test/resources/test_files/payments/xml";
 
     @Bean(name = "xml-doc-parser")
     public DocParser getXmlDocParser() {
         return new XmlDocParser();
     }
 
-    @Bean(name="test-xml-file")
+    @Bean(name = "test-xml-file")
     public File testXmlFile() {
         File file = new File(XML_FILE_LOCATION);
         if (!file.exists())
@@ -187,7 +227,7 @@ class XmlDocParserTestConfig {
         return file;
     }
 
-    @Bean(name="test-xml-directory")
+    @Bean(name = "test-xml-directory")
     public File testXmlDirectory() {
         File file = new File(XML_FILES_DIRECTORY_LOCATION);
         if (!file.exists() || !file.isDirectory())
