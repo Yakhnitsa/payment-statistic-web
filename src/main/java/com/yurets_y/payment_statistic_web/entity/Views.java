5c7914b70923d03eadea926a9c89d072c1439afd
Yakhnitsa <transpmailbox@gmail.com>
2020-11-22 17:58:18 +0200

    Before JsonViews refactoring

--- src/main/java/com/yurets_y/payment_statistic_web/entity/Views.java
+++ src/main/java/com/yurets_y/payment_statistic_web/entity/Views.java
@@ -1,14 +1,27 @@
 package com.yurets_y.payment_statistic_web.entity;
 
 public class Views {
+    /*
+    * Только базовые поля для загрузки информации в сводках, и изначально загруженных данных
+    * */
     public interface ShortView{
 
     }
 
+    /*
+    * Расширенное отображение сущности, включая сущности один к одному, и не включая сущности один к многим
+    * */
     public interface NormalView extends  ShortView{
 
     }
+    /* Расширенное отображение, включает отображение сущностей один к многим
+    * */
+    public interface ExtendedView extends NormalView{
 
+    }
+    /*
+    * Отображение всех полей, кроме полей аудитинга.
+    * */
     public interface FullView extends NormalView {
 
     }
