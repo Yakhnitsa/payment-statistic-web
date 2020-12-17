19ece6612dd2f959a77bed77c46232c5beb21b7f
Yuriy work-pc <transpmailbox@gmail.com>
2020-12-17 13:38:54 +0200

    before payment details controller refactoring

--- src/main/java/com/yurets_y/payment_statistic_web/service/payment_statistic_services/PaymentDetailsSpecification.java
+++ src/main/java/com/yurets_y/payment_statistic_web/service/payment_statistic_services/PaymentDetailsSpecification.java
@@ -15,7 +15,7 @@ import java.util.Date;
 @Component
 public class PaymentDetailsSpecification {
 
-    public Specification<PaymentDetails> dateSpecification(Date dateFrom, Date dateTo){
+    public Specification<PaymentDetails> dateSpec(Date dateFrom, Date dateTo){
         return new Specification<PaymentDetails>() {
             @Override
             public Predicate toPredicate(Root<PaymentDetails> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
@@ -28,6 +28,43 @@ public class PaymentDetailsSpecification {
                     return predicate;
                 }
                 Predicate pr = criteriaBuilder.lessThanOrEqualTo(root.<Date>get("date"), dateTo);
+                return pr;
+
+            }
+        };
+    }
+
+    public Specification<RailroadDocument> payerCodeSpec(Integer payerCode){
+        return (Specification<RailroadDocument>) (root, criteriaQuery, criteriaBuilder)
+                -> payerCode == null ? criteriaBuilder.and() :
+                criteriaBuilder.equal(root.get("paymentList").get("payerCode"), payerCode);
+    }
+
+    public Specification<RailroadDocument> stationSpec(Integer stationCode){
+        return (Specification<RailroadDocument>) (root, criteriaQuery, criteriaBuilder)
+                -> stationCode == null ? criteriaBuilder.and() :
+                criteriaBuilder.equal(root.get("stationCode"),stationCode);
+    }
+
+    public Specification<RailroadDocument> paymentTypeSpec(String paymentType){
+        return (Specification<RailroadDocument>) (root, criteriaQuery, criteriaBuilder)
+                -> paymentType == null || paymentType.equals("") ? criteriaBuilder.and() :
+                criteriaBuilder.equal(root.get("type"),paymentType);
+    }
+
+    public Specification<PaymentDetails> totalPaymentSpec(Long higherThan, Long lessThan){
+        return new Specification<PaymentDetails>() {
+            @Override
+            public Predicate toPredicate(Root<PaymentDetails> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                if(higherThan == null && lessThan == null) return criteriaBuilder.and();
+                if(higherThan != null && lessThan != null){
+                    return criteriaBuilder.between(root.get("totalPayment"),higherThan,lessThan);
+                }
+                if(higherThan != null){
+                    Predicate predicate = criteriaBuilder.greaterThanOrEqualTo(root.get("totalPayment"), higherThan);
+                    return predicate;
+                }
+                Predicate pr = criteriaBuilder.lessThanOrEqualTo(root.get("totalPayment"), lessThan);
                 return pr;
 
             }
