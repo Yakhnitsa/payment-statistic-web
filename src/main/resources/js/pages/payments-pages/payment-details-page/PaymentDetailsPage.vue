d0b515cae2759631acdf71100d930caa1ff18502
Yuriy work-pc <transpmailbox@gmail.com>
2020-12-18 12:03:18 +0200

    change form style

--- src/main/resources/js/pages/payments-pages/payment-details-page/PaymentDetailsPage.vue
+++ src/main/resources/js/pages/payments-pages/payment-details-page/PaymentDetailsPage.vue
@@ -15,13 +15,13 @@
 
                     <div class="collapse show my-2" id="requestForm">
                         <div class="form-row">
-                            <div class="form-group col-lg-2">
+                            <div class="form-group col-lg-2 col-md-4">
                                 <label for="inputPayerCode">Код плательщика</label>
                                 <select v-model="payerCode" class="form-control form-control-sm" id="inputPayerCode">
                                     <option v-for="code in paymentCodes">{{code}}</option>
                                 </select>
                             </div>
-                            <div class="form-group col-lg-4">
+                            <div class="form-group col-lg-4 col-md-8">
                                 <label for="inputPaymentType">Название платежа</label>
                                 <input type="text" v-model="paymentType"
                                        class="form-control" id="inputPaymentType"
@@ -31,18 +31,18 @@
                                     <option v-for="type in foundTypes" :value="type"></option>
                                 </datalist>
                             </div>
-                            <div class="form-group col-lg-2">
+                            <div class="form-group col-lg-3 col-md-6">
                                 <label for="inputDateFrom">Начало периода</label>
                                 <input type="date" v-model="dateFrom" class="form-control" id="inputDateFrom">
                             </div>
-                            <div class="form-group col-lg-2">
+                            <div class="form-group col-lg-3 col-md-6">
                                 <label for="inputPassword4">Конец периода</label>
                                 <input type="date" v-model="dateUntil" class="form-control" id="inputPassword4">
                             </div>
                         </div>
                         <div class="form-row">
 
-                            <div class="form-group col-md-3">
+                            <div class="form-group col-lg-3 col-md-6">
                                 <label for="inputStation">Станция ({{stationCode}})</label>
                                 <station-input :input-class="'form-control'"
                                                :stationCode="stationCode"
@@ -52,17 +52,17 @@
                                 <!--<input type="text" v-model="stationCode" class="form-control" id="inputStation"-->
                                        <!--placeholder="(код) Станция">-->
                             </div>
-                            <div class="form-group col-md-3">
+                            <div class="form-group col-lg-3 col-md-6">
                                 <label for="inputDocNumber">Документ</label>
                                 <input type="text" v-model="docNumber" class="form-control" id="inputDocNumber"
                                        placeholder="№ документа">
                             </div>
-                            <div class="form-group col-md-2">
+                            <div class="form-group col-lg-3 col-md-6">
                                 <label for="inputPaymentSumFrom">Мин. сумма платежа</label>
                                 <input type="number" v-model="paymentSumFrom" class="form-control" id="inputPaymentSumFrom"
                                        placeholder="Сумма с НДС">
                             </div>
-                            <div class="form-group col-md-2">
+                            <div class="form-group col-lg-3 col-md-6">
                                 <label for="inputPaymentSumTo">Макс. сумма платежа</label>
                                 <input type="number" v-model="paymentSumTo" class="form-control" id="inputPaymentSumTo"
                                        placeholder="Сумма с НДС">
